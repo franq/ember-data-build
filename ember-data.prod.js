86634cfdab4cc077ec0a8057e33e788fbd7d3c49
Sohum Banerjea <sohumb@gmail.com>
2014-12-23 11:31:18 +1100

    2753ea73816f214a5a0c7aba22ab1d550d9f1877

--- ember-data.prod.js
+++ ember-data.prod.js
@@ -4352,12 +4352,12 @@
     /**
       @property VERSION
       @type String
-      @default '<%= versionStamp %>'
+      @default '1.0.0-beta.13b'
       @static
     */
     /*jshint -W079 */
     var ember$data$lib$core$$DS = Ember.Namespace.create({
-      VERSION: '<%= versionStamp %>'
+      VERSION: '1.0.0-beta.13b'
     });
 
     if (Ember.libraries) {
