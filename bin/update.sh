7b930c2dee90443a7f9c26d525bd585b84d5aecb
Sohum Banerjea <sohumb@gmail.com>
2014-12-23 11:19:06 +1100

    update.sh now tags the right versionStamp

--- bin/update.sh
+++ bin/update.sh
@@ -11,5 +11,6 @@ sha=$(cd .. && git rev-parse HEAD)
 (cd .. && npm run dist) \
   && cp ../dist/*.* . \
   && find . -name '*.json' -exec perl -pi -e "s/VERSION_STRING_PLACEHOLDER/$version/g" {} \; \
+  && find . \(  -name '*.js' -o -name '*.map' -o -name '*.json' \) -exec perl -pi -e "s/<%= versionStamp %>/$version/g" {} \; \
   && git commit -m "$sha" -a \
   && git tag "v$version"
